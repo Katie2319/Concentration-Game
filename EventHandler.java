ed374d9f21e2b7c11f86d550d9697b6773218b45
MelatAssefa <49922671+MelatAssefa@users.noreply.github.com>
2019-04-23 16:44:41 -0400

    Update EventHandler.java

--- EventHandler.java
+++ EventHandler.java
@@ -4,8 +4,11 @@ class ButtonClickHandler implements EventHandler<ActionEvent> {
    
    public void handle(ActionEvent event) {
 
-
-
+   for (int i=0;i<4;i++){ 
+  
+       if(event.getSource().equals(buttons[i])){
+          buttons[i] = makeButton(image);
+          grid.add(buttons[i],i,0);
 
 }
 

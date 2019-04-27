bb6070d4da0c2cadd71529a9a28efa6d515152a5
okeefebl <49697584+okeefebl@users.noreply.github.com>
2019-04-27 09:31:05 -0400

    Update MemoryGame.java

--- Concentration/MemoryGame.java
+++ Concentration/MemoryGame.java
@@ -5,54 +5,83 @@ class MemoryGame implements Game{
   boolean isMatch;
   boolean isValid;
   String gameStatus;
-  int boardSize;
+  int boardSize = 2;
   int turnCount;
+  boolean match;
+  int matchCount = 0;
+  
   int[][] card1 = new int[boardSize][boardSize];
   int[][] card2 = new int[boardSize][boardSize]; 
-  int[][] secretBoard = new int[boardSize][boardSize]; 
-   
+  int[][] secretBoard = new int[boardSize][boardSize];
+ 
    //Default constructor
    public void setUp(){
       isWinner = false;
       boardSize = 2;
       turnCount = 0;
+   }
+  //Set up the hidden board 
+   public void setBoard(){ 
       secretBoard[0][0] = 1;
       secretBoard[0][1] = 1;
       secretBoard[1][0] = 2;
-      secretBoard[1][1] = 2;
-      }
-      
+      secretBoard[1][1] = 2; 
+  }  
+   
    //Accessors
    public boolean isWinner(){
-      return isWinner;
-   }
+         return isWinner;
+   }//isWinner
    
    public int getTurnCnt(){
       return turnCount;
-   }
+   }//getTurnCount
+   
+   public int getMatchCount(){
+      return matchCount;
+   }//getMatchCount
+      
    //Mutators
-   //needs work
+
    public void takeTurn(int[] x){
       turnCount++;
-      card1[x[0]][x[1]] = 1;
-      card2[x[2]][x[3]] = 1;
-   }
-   
+   }//takeTurn
+         
    public boolean isValidInput(int [] x){
       isValid = true;
       return isValid;
-   }   
+   } //isValidInput  
          
    public String gameOverStatus(){
       if(isWinner == true)
          return("won");
       else
          return("NA");
-   }
+   }//gameOverStatus
+   
    public boolean isMatch(int[] x){
-      if(card1[x[0]][x[1]] == secretBoard[x[0]][x[1]])
-        return true;  
+      if(secretBoard[x[0]][x[1]] == secretBoard[x[2]][x[3]]){
+        match = true;
+        return match;  
+      }
       else 
-        return false;
-     }  
+        match = false;
+        return match;
+   }//isMatch
+   
+   public int matchCount(){
+      if(match == true){
+         matchCount++;
+      }   
+      return matchCount;
+   }
+        
+   public void checkWinner(){
+      if(matchCount >= 2){
+         isWinner = true;
+      }
+      else
+         isWinner = false;
+   }//checkWinner  
+     
 }// Game class
